0717d5140bccd9a5b48912c3ccd042ffcc34c1a2
Omer Gul <omergul2001@gmail.com>
2022-08-29 21:31:09 +1000

    saving

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import anchormanVid from "./assets/anchormanVid.mp4"
-import {TextField} from '@mui/material'
+import {TextField, Card, CardContent, Typography, CardActions, Button, Grid} from '@mui/material'
+import React from 'react';
+import OutlinedCard from './components/resultCard';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="wrapper"> */}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p className="title-text">
-          Welcome to Anchorman. The devs are on it...
-        </p> */}
-      
+     
       <header className="wrapper"> </header>
       <video src={anchormanVid} autoPlay loop muted/>
 
@@ -21,21 +18,28 @@ function App() {
       <div className="content">
         
           <h1>Anchorman</h1>
+          
           <TextField  size="small" 
-          className="search" 
-          id="search" 
-          label="Today's top story..." 
-          variant="outlined"   
-          inputProps={{ style: { fontFamily: 'Arial, Helvetica, sans-serif', color: 'white'}}}
+            className="search" 
+            id="search" 
+            label="Today's top story is about..." 
+            variant="outlined"   
+            inputProps={{ style: { fontFamily: 'Arial, Helvetica, sans-serif', color: 'white'}}}
           />
+
           <p>60% of the time it works everytime...</p>
        
-      </div>
- 
+        {/* <Grid>
+        <OutlinedCard result="asdgf"/>
+        <OutlinedCard result="asdgf"/>
+        <OutlinedCard result="asdgf"/>
+        </Grid> */}
       
-     
-        
-      {/* </header> */}
+
+      </div>
+
+      
+
     </div>
   );
 }
